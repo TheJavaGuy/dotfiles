c4db9d65d375ead252a63b3e79d3d3181c2b2d97
Ivan Milosavljevic (TheJavaGuy) <TheJavaGuy@yandex.com>
2019-11-20 22:08:47 +0100

    add gitconfig, tmux, vim, zsh, kitty config, brew script

---
+++ brew.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install zsh
+brew install zsh-completions
+brew install git
+brew install python
+brew install jrnl
+brew install fzf
+(brew --prefix)/opt/fzf/install
+brew install ag
+brew install tmux
+brew cask install iterm2
+brew cask install atom
+brew cask install firefox
+brew cask install vivaldi
+brew tap homebrew/cask-versions
+brew tap adoptopenjdk/openjdk
+brew cask install adoptopenjdk/openjdk/adoptopenjdk8
+brew cask install adoptopenjdk11
+brew install jenv
+jenv add /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+jenv add /Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/
+jenv global 1.8.0.232
+brew install ant
+brew install maven
+brew install gradle
+brew cask install intellij-idea
+brew cask install sourcetree
+brew cask install foxitreader
+brew cask install double-commander
+brew tap homebrew/cask-drivers
+brew cask install evoluent-vertical-mouse-device-controller
+brew cask install telegram
+brew cask install mat
+brew cask install docker
+brew cask install visual-studio-code
--- vim/.vimrc
+++ vim/.vimrc
@@ -1,1 +1,11 @@
 set number
+noremap : ;
+noremap ; :
+syntax on
+:hi gitcommitSummary ctermfg=yellow ctermbg=red
+colorscheme peachpuff
+set wrap linebreak nolist
+set textwidth=0
+set wrapmargin=0
+set laststatus=2
+set statusline+=%F\ %l\:%c\:%v\ %m\ %r\ %y
